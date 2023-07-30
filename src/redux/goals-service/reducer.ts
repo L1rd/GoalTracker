d74512eaf0bda9bd4037156268d47d3d60f4552a
Artem Riabokon <artemriabokon17@gmail.com>
2023-07-30 13:42:06 +0300

    add static preview

--- src/redux/goals-service/reducer.ts
+++ src/redux/goals-service/reducer.ts
@@ -53,7 +53,119 @@ const initialState: IGoalReducer = {
 	theme: 'theme-dark',
 	currentPage: '',
 	language: 'en',
-	roadmaps: [],
+	roadmaps: [
+		{
+			title: 'Learning Front-end',
+			createdAt: '30/07/2023',
+			author: 'Artem',
+			stackOfTechnology: 'Front-end',
+			goals: [
+				{
+					title: 'Learn Styles Tools',
+					category: 'Personal',
+					priority: 4,
+					start: '30/05/2023',
+					end: '30/06/2023',
+					tasks: [
+						{ time: '12:58', title: 'CSS' },
+						{ time: '12:58', title: 'SCSS/SASS' },
+						{ time: '12:58', title: 'Styled-Components/Stitches' },
+						{ time: '12:58', title: 'Material UI' },
+					],
+					subgoals: [],
+					type: 'Multi-step',
+					progress: 0,
+					status: 'in-progress',
+				},
+				{
+					title: 'Learn React',
+					category: 'Personal',
+					priority: 4,
+					start: '30/06/2023',
+					end: '30/07/2023',
+					tasks: [],
+					subgoals: [
+						{
+							end: '30/07/2023',
+							progress: 0,
+							start: '29/07/2023',
+							title: 'Main Concepts',
+							tasks: [
+								{
+									time: '12:07',
+									title: 'Introducing JSX',
+								},
+								{
+									time: '12:07',
+									title: 'Rendering Elements',
+								},
+								{
+									time: '12:07',
+									title: 'Components and Props',
+								},
+								{
+									time: '12:07',
+									title: 'State and Lifecycle',
+								},
+								{
+									time: '12:07',
+									title: 'Handling Events',
+								},
+							],
+						},
+						{
+							end: '30/07/2023',
+							progress: 0,
+							start: '29/07/2023',
+							title: 'Hooks',
+							tasks: [
+								{
+									time: '12:07',
+									title: 'Introducing Hooks',
+								},
+								{
+									time: '12:07',
+									title: 'Hooks at a Glance',
+								},
+								{
+									time: '12:07',
+									title: 'Using the State Hook',
+								},
+								{
+									time: '12:07',
+									title: 'Using the Effect Hook',
+								},
+								{
+									time: '12:07',
+									title: 'Rules of Hooks',
+								},
+							],
+						},
+					],
+					type: 'Multi-step',
+					progress: 0,
+					status: 'in-progress',
+				},
+				{
+					title: 'Learn Redux',
+					category: 'Personal',
+					priority: 4,
+					start: '30/07/2023',
+					end: '30/08/2023',
+					tasks: [
+						{ time: '12:58', title: 'Introduction' },
+						{ time: '12:58', title: 'Tutorials' },
+						{ time: '12:58', title: 'Using Redux Toolkit' },
+						{ time: '12:58', title: 'API Reference' },
+					],
+					subgoals: [],
+					type: 'Multi-step',
+					progress: 0,
+					status: 'in-progress',
+				},
+			],
+		},
+	],
 };
 
 export const goalsSlice = createSlice({
